cce6c1284e7333b63e5e5adfe4d7e2119d45037f
RR <rizwan345300@gmail.com>
2024-09-19 16:27:51 +0500

    3rd commit

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,10 @@
 const file = () =>{
     console.log("File uploaded successfully");
 }
+const fun1 = () => {
+    console.log("Function 1 executed");
+    fun2();
+}
 const fun2 = () =>{
     console.log("Function 2 executed");
 }
